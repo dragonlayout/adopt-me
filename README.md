5dd6c64105975b2debdb962773b01fd2294f2221
chenlong <dragonlayoutt@gmail.com>
2022-11-14 16:45:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Adopt Me
+
+source code of [COMPLETE INTRO TO REACT V8](https://react-v8.holt.courses/)
---
+++ src/SearchParams.jsx
@@ -0,0 +1,18 @@
+import {useState} from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input id="location" value={location} placeholder="Location" />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
